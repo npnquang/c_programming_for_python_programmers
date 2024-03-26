2e7183f3e51abefc0af68cc582f41f93bc7ef869
Joseph John <joseph.john@anu.edu.au>
2024-03-26 15:35:59 +1100

    pointer arithmatic

--- src/6_pointers/2_pointers.c
+++ src/6_pointers/2_pointers.c
@@ -3,9 +3,16 @@
 void main()
 {
     int x = 10;
-    int *ptr = &x;
+    int *ptr_i = &x;
 
-    printf("ptr      = %u \n", ptr);
-    printf("ptr + 1  = %u \n", (ptr + 1));
-    printf("ptr - 1  = %u \n", (ptr - 1));
+    printf("\n \nptr      = %u \n", ptr_i);
+    printf("ptr + 1  = %u \n", (ptr_i + 1));
+    printf("ptr - 1  = %u \n", (ptr_i - 1));
+
+    char c = 10;
+    char *ptr_c = &c;
+
+    printf("\n \nptr      = %u \n", ptr_c);
+    printf("ptr + 1  = %u \n", (ptr_c + 1));
+    printf("ptr - 1  = %u \n", (ptr_c - 1));
 }

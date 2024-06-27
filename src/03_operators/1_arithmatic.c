35d9f82280844d4c54db34b97493b9b8655ca6f3
npnquang <nguyenjoe0201@gmail.com>
2024-06-27 16:01:09 +0700

    Update the string and arithmatic in 1_arithmatic.c

--- src/03_operators/1_arithmatic.c
+++ src/03_operators/1_arithmatic.c
@@ -6,8 +6,8 @@ void main()
 {
     int a = 7, b = 2;
     
-    printf("a + b = %d \n", a / b);
+    printf("a + b = %d \n", a + b);
     printf("a * b = %d \n", a * b);
-    printf("a * b = %lf \n", (float)a / b);
-    printf("a * b = %d \n", a / b);
+    printf("a / b = %lf \n", (float)a / b);
+    printf("a mod b = %d \n", a / b);
 }
